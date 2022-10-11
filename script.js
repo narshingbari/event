748eb90ff70dbd2b18c1ba29150d87820506f360
Saddam Hussain <33945951+narshingbari@users.noreply.github.com>
2022-10-11 09:53:55 +0530

    urls

--- script.js
+++ script.js
@@ -9,9 +9,21 @@ $('body').vegas({
   animation: 'random',
   animationDuration: 20000,
   slides: [
-    { src: 'https://ununsplash.imgix.net/reserve/RONyPwknRQOO3ag4xf3R_Kinsey.jpg?fit=crop&fm=jpg&h=700&q=75&w=1600' },
-    { src: 'https://unsplash.imgix.net/photo-1414438992182-69e404046f80?fit=crop&fm=jpg&h=625&q=75&w=1600' },
-    { src: 'https://unsplash.imgix.net/photo-1414490929659-9a12b7e31907?fit=crop&fm=jpg&h=800&q=75&w=1600' },
-    { src: 'https://unsplash.imgix.net/uploads/14129863345840df499fc/0165574c?fit=crop&fm=jpg&h=600&q=75&w=1600' }
+    { src: 'https://i.postimg.cc/SNFwbSFB/IMG-20200216-123950.jpg' },
+    { src: 'https://i.postimg.cc/vT66d79d/Whats-App-Image-2022-07-25-at-9-35-42-PM-1.jpg' },
+    { src: 'https://i.postimg.cc/mrX4N0nm/Narshingbari-Saddam-Computer-Cource-Goa.jpg' },
+    { src: 'https://i.postimg.cc/MT3ywKF1/unnamed-615x300.jpg' },
+    { src: 'https://i.postimg.cc/4xv2gVbt/Tamil-nadu-1.jpg' },
+    { src: 'https://i.postimg.cc/MGWRjQky/Narshingbari-Saddam-Computer-Cource.jpg' },
+    { src: 'https://i.postimg.cc/GtNJFhPb/IMG-20211122-140129.jpg' },
+    { src: 'https://i.postimg.cc/mDLwRn2W/IMG-20200825-060603.jpg' },
+    { src: 'https://i.postimg.cc/8P4TzJH8/IMG-20200525-104358.jpg' },
+    { src: 'https://i.postimg.cc/Fs1q5Y7s/Face-App-1571693827672.jpg' },
+    { src: 'https://i.postimg.cc/FHJmgWvv/IMG-20200216-123357.jpg' },
+    { src: 'https://i.postimg.cc/tTBjk8mr/IMG-20200216-122647.jpg' },
+    { src: 'https://i.postimg.cc/D0z2K7Xk/IMG-20200216-121745.jpg' },
+    { src: 'https://i.postimg.cc/DzWySvj2/IMG-20181125-091245.jpg' },
+    { src: 'https://i.postimg.cc/fLJXTQXX/images-1.jpg' },
+    { src: 'https://i.postimg.cc/BnB2mg48/Face-App-1571697376560.jpg' }
   ]
 });
